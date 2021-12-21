461ae4f8df71b6a29b3fd4e8f617764f2c7abd20
annelisemarais <maraisannelise98@gmail.com>
2021-12-21 10:22:40 +0100

    Update EEG_Preprocess.m
    Add Rereferencing to average scalp

--- EEG_Preprocess.m
+++ EEG_Preprocess.m
@@ -1,6 +1,5 @@
 %Code : Anne-Lise Marais
 %Preprocessing (filtering and artifact rejection) : Anne-Lise Marais or Marie Anquetil or Victoria Dumont w/ Netstation EGI
-%Data acquisition : Marie Anquetil or Anne-Lise Marais or Victoria Dumont
 %Protocol : Anne-Lise Marais
 
 %This code preprocesses the data of an oddball somatosensory protocol for the DECODE research. See https://www.unicaen.fr/projet_de_recherche/decode/
@@ -21,7 +20,7 @@ save '...\subjcode_Raw.mat' 'subjcode_Raw'
 %%
 %%Find the index of the first tag in the NS software segmented data
 %Load the software segmented data
-load '...subjcode_Segmented.mat'
+load '...\subjcode_Segmented.mat'
 %Find the measure at the first tag (in the softwar segmentation)
 Offset_measure_NS = Fam_Segment_001(:,100)';
 %Get the raw data
@@ -29,18 +28,18 @@ Offset_measure_Brut = (subjcode_Raw)';
 %Find the measure of the first tag in the raw data. Will return the index off the first tag in raw data.
 Offset_measure_Idx = find(ismember(Offset_measure_Brut,Offset_measure_NS,'rows'));
 %
-%Get the Tag time and tag idex from the relative event table
+%Get the Tag time and tag idex from the relative event table from Netstation
 subjcode_event=readtable('...\subjcode_Event.xlsx');
 subjcode_Relative_Tag_Idx = round(subjcode_event.Tag_time);
 subjcode_Tag_Number = round(subjcode_event.Tag_number);
 %Correct the relative event table by substracting the offset NS data
-Offset_measure = subjcode_Relative_Tag_Idx(1,1) - Offset_measure_Idx(1,1)
+Offset_measure = subjcode_Relative_Tag_Idx(1,1) - Offset_measure_Idx(1,1);
 subjcode_Tag_Idx = subjcode_Relative_Tag_Idx - Offset_measure ;
-save '...\subjcode_Event.mat' 'subjcode_Tag_Idx' 'subjcode_Tag_Number' 'Offset_measure'
+save '...\subjcode_Event.mat' 'subjcode_Tag_Idx' 'subjcode_Tag_Number'
 clear
 %%
 load ...\subjcode_Raw.mat
-load ...subjcode_Event.mat
+load ...\subjcode_Event.mat
 %%
 %Segmentation
 %%
@@ -83,7 +82,7 @@ end
 %%
 Idx_Segment_Fam_subjcode = Idx_Segment_Fam_subjcode';
 Idx_Segment_Omi_subjcode = Idx_Segment_Omi_subjcode';
-Idx_Segment_Std_subjcode = Idx_Segment_Std_subjcode;
+Idx_Segment_Std_subjcode = Idx_Segment_Std_subjcode';
 Idx_Segment_Deviant_subjcode = Idx_Segment_Deviant_subjcode';
 Idx_Segment_StimMoy_subjcode = Idx_Segment_StimMoy_subjcode';
 Idx_Segment_PostOm_subjcode = Idx_Segment_PostOm_subjcode';
@@ -112,22 +111,6 @@ clear
 load ...\subjcode_Segmented_ML.mat
 %%
 %ARTIFACT DETECTION
-%%
-%Hand made (I check every segment one by one, error risk +++)
-%%
-%Segments_Con_AD_subjcode = [Segments_Con_subjcode(:,:,[2 3 4 5 6 7 9 11 12 13 19 20 23 24 25 26 27 28 29 30 32 34 35 39 40])];
-%Rejected_Segment_Con_subjcode = [Segments_Con_subjcode(:,:,[1 8 10 14 15 16 17 18 21 22 31 33 36 37 38])];
-%Segments_Deviant_AD_subjcode = [Segments_Deviant_subjcode(:,:,[1 2 4 5 6 9 10 11 15 19 20 22 24 25 26 28 29])];
-%Rejected_Segment_Deviant_subjcode = [Segments_Deviant_subjcode(:,:,[3 7 8 12 13 14 16 17 18 21 23 27 30])];
-%Segments_Fam_AD_subjcode = [Segments_Fam_subjcode(:,:,[1 2 3 4 6 7 9 11 12 13 14 15 17 18 19 20 21 22 23 25 26 27 28 31 32 35 37])];
-%Rejected_Segment_Fam_subjcode = [Segments_Fam_subjcode(:,:,[5 8 10 16 24 29 30 33 34 36 38 39 40])];
-%Segments_Omission_AD_subjcode = [Segments_Omission_subjcode(:,:,[1 4 5 6 9 10 14 15 16 19 20 21 23 24 26 27 28])];
-%Rejected_Segment_Omission_subjcode = [Segments_Omission_subjcode(:,:,[2 3 7 8 11 12 13 17 18 22 25 29 30])];
-%Segments_PostOm_AD_subjcode = [Segments_PostOm_subjcode(:,:,[1 2 4 5 6 8 9 10 12 14 15 16 18 19 20 21 23 24 26 27 28 30])];
-%Rejected_Segment_PostOm_subjcode = [Segments_PostOm_subjcode(:,:,[3 7 11 13 17 22 25 29])];
-%Segments_Std_AD_subjcode = [Segments_Std_subjcode(:,:,[3 4 5 6 9 10 11 12 13 14 15 17 20 21 22 23 24 26 27 28 29 30 31 33 35 36 40 41 42 43 44 47 48 49 52 54 56 57 59 62 63 65 66 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90])];
-%Rejected_Segment_Std_subjcode = [Segments_Std_subjcode(:,:,[1 2 7 8 16 18 19 25 32 34 37 38 39 45 46 50 51 53 55 58 60 61 64 67])];
-
 %%
 %With Netstation
 %I do the artifact rejection using Netstation and use the log file output as a logical to reject bad segments
@@ -217,35 +200,6 @@ end
 %Without netstation
 %Make Matlab learn what an artifact is and automatically reject it
 %%
-%plot(Segments_Con_subjcode(:,3:47,18))
-%ylim([-5,5])
-%ylabel('µV')
-%xlim([0,1000])
-%xticks([0 100 120 150 180 200 240 300 400 500 600 700 800 900 1000])
-%xticklabels({'-100','0','20', '50', '80','100', '140','200','300', '400','500','600','700', '800', '900'})
-%xlabel('ms')
-%set(gca,'YDir','reverse')
-%%
-%Rejected_Segment_Fam_subjcode = [];
-%Rejected_Segment_Omi_subjcode = [];
-%Rejected_Segment_Std_subjcode = [];
-%Rejected_Segment_Deviant_subjcode = [];
-%Rejected_Segment_StimMoy_subjcode = [];
-%Rejected_Segment_PostOm_subjcode = [];
-%Rejected_Segment_Con_subjcode = [];
-
-%Il faut sortir une loop 3D pas une 2D !!
-
-%for i = 1:(size(Segments_Con_subjcode,3))
-%    if Segments_Con_subjcode(:,:, i) > 6
-%        Rejected_Segment_Con_subjcode = [Rejected_Segment_Con_subjcode; Segments_Con_subjcode]
-%    elseif Segments_Con_subjcode(:,:, i) < -6
-%        Rejected_Segment_Con_subjcode = [Rejected_Segment_Con_subjcode; Segments_Con_subjcode]
-%    else
-%        Segments_Con_subjcode = Segments_Con_subjcode
-%    end
-%end
-%%
 save 'subjcode_Good_Segments.mat' Segments_AD_*
 save 'subjcode_Bad_Segments.mat' Rejected_*
 clear
@@ -254,71 +208,40 @@ load subjcode_Good_Segments.mat
 %%
 %RE REFERENCING TO AVERAGE (each data point is substracted by the average of the electrode)
 %%
-%Average_reference_Con_subjcode = mean2(Segments_AD_Con_subjcode);
-%Average_reference_Fam_subjcode = mean2(Segments_AD_Fam_subjcode);
-%Average_reference_Deviant_subjcode = mean2(Segments_AD_Deviant_subjcode);
-%Average_reference_Std_subjcode = mean2(Segments_AD_Std_subjcode);
-%Average_reference_StimMoy_subjcode = mean2(Segments_AD_StimMoy_subjcode);
-%Average_reference_PostOm_subjcode = mean2(Segments_AD_PostOm_subjcode);
-%Average_reference_Omission_subjcode = mean2(Segments_AD_Omission_subjcode);
-
-%Segments_Ref_Con_subjcode = Segments_AD_Con_subjcode - Average_reference_Con_subjcode;
-%Segments_Ref_Fam_subjcode = Segments_AD_Fam_subjcode - Average_reference_Fam_subjcode;
-%Segments_Ref_Deviant_subjcode = Segments_AD_Deviant_subjcode - Average_reference_Deviant_subjcode;
-%Segments_Ref_Std_subjcode = Segments_AD_Std_subjcode - Average_reference_Std_subjcode;
-%Segments_Ref_StimMoy_subjcode = Segments_AD_StimMoy_subjcode - Average_reference_StimMoy_subjcode;
-%Segments_Ref_PostOm_subjcode = Segments_AD_PostOm_subjcode - Average_reference_PostOm_subjcode;
-%Segments_Ref_Omission_subjcode = Segments_AD_Omission_subjcode - Average_reference_Omission_subjcode;
+mean_RR_Con_subjcode = mean(Segments_AD_Con_subjcode(1:128,:,:));
+RR_Con_subjcode = Segments_AD_Con_subjcode - mean_RR_Con_subjcode;
+mean_RR_Fam_subjcode = mean(Segments_AD_Fam_subjcode(1:128,:,:));
+RR_Fam_subjcode = Segments_AD_Fam_subjcode - mean_RR_Fam_subjcode;
+mean_RR_Deviant_subjcode = mean(Segments_AD_Deviant_subjcode(1:128,:,:));
+RR_Deviant_subjcode = Segments_AD_Deviant_subjcode - mean_RR_Deviant_subjcode;
+mean_RR_Omission_subjcode = mean(Segments_AD_Omission_subjcode(1:128,:,:));
+RR_Omission_subjcode = Segments_AD_Omission_subjcode - mean_RR_Omission_subjcode;
+mean_RR_PostOm_subjcode = mean(Segments_AD_PostOm_subjcode(1:128,:,:));
+RR_PostOm_subjcode = Segments_AD_PostOm_subjcode - mean_RR_PostOm_subjcode;
+mean_RR_Std_subjcode = mean(Segments_AD_Std_subjcode(1:128,:,:));
+RR_Std_subjcode = Segments_AD_Std_subjcode - mean_RR_Std_subjcode;
+mean_RR_StimMoy_subjcode = mean(Segments_AD_StimMoy_subjcode(1:128,:,:));
+RR_StimMoy_subjcode = Segments_AD_StimMoy_subjcode - mean_RR_StimMoy_subjcode;
 %%
 %BASELINE CORRECTION
 %For each channel, the average of all the samples within the baseline interval (100ms) 
 %is subtracted from every sample in the segment
 %%
-Con_3D_subjcode = [];
-Fam_3D_subjcode = [];
-Deviant_3D_subjcode = [];
-Std_3D_subjcode = [];
-PostOm_3D_subjcode = [];
-StimMoy_3D_subjcode = [];
-Omission_3D_subjcode = [];
+mean_Baseline_Con_subjcode = mean(RR_Con_subjcode(:,1:99,:),2);
+Con_3D_subjcode = RR_Con_subjcode - mean_Baseline_Con_subjcode;
+mean_Baseline_StimMoy_subjcode = mean(RR_StimMoy_subjcode(:,1:99,:),2); 
+StimMoy_3D_subjcode = RR_StimMoy_subjcode - mean_Baseline_StimMoy_subjcode;
+mean_Baseline_Std_subjcode = mean(RR_Std_subjcode(:,1:99,:),2); 
+Std_3D_subjcode = RR_Std_subjcode - mean_Baseline_Std_subjcode;
+mean_Baseline_PostOm_subjcode = mean(RR_PostOm_subjcode(:,1:99,:),2); 
+PostOm_3D_subjcode = RR_PostOm_subjcode - mean_Baseline_PostOm_subjcode;
+mean_Baseline_Omission_subjcode = mean(RR_Omission_subjcode(:,1:99,:),2); 
+Omission_3D_subjcode = RR_Omission_subjcode - mean_Baseline_Omission_subjcode;
+mean_Baseline_Fam_subjcode = mean(RR_Fam_subjcode(:,1:99,:),2); 
+Fam_3D_subjcode = RR_Fam_subjcode - mean_Baseline_Fam_subjcode;
+mean_Baseline_Deviant_subjcode = mean(RR_Deviant_subjcode(:,1:99,:),2); 
+Deviant_3D_subjcode = RR_Deviant_subjcode - mean_Baseline_Deviant_subjcode;
 
-
-for i = 1:(size(Segments_AD_Con_subjcode,3))
-    Segment_Con_Bas_subjcode = Segments_AD_Con_subjcode(:,:,i) - mean(Segments_AD_Con_subjcode(:,1:99,i),2);
-    Con_3D_subjcode = cat(3,Con_3D_subjcode, Segment_Con_Bas_subjcode);
-end
-
-for i = 1:(size(Segments_AD_Fam_subjcode,3))
-    Segment_Fam_Bas_subjcode = Segments_AD_Fam_subjcode(:,:,i) - mean(Segments_AD_Fam_subjcode(:,1:99,i),2);
-    Fam_3D_subjcode = cat(3,Fam_3D_subjcode, Segment_Fam_Bas_subjcode);
-end
-
-for i = 1:(size(Segments_AD_Deviant_subjcode,3))
-    Segment_Deviant_Bas_subjcode = Segments_AD_Deviant_subjcode(:,:,i) - mean(Segments_AD_Deviant_subjcode(:,1:99,i),2);
-    Deviant_3D_subjcode = cat(3,Deviant_3D_subjcode, Segment_Deviant_Bas_subjcode);
-end
-
-for i = 1:(size(Segments_AD_Std_subjcode,3))
-    Segment_Std_Bas_subjcode = Segments_AD_Std_subjcode(:,:,i) - mean(Segments_AD_Std_subjcode(:,1:99,i),2);
-    Std_3D_subjcode = cat(3,Std_3D_subjcode, Segment_Std_Bas_subjcode);
-end
-
-for i = 1:(size(Segments_AD_PostOm_subjcode,3))
-    Segment_PostOm_Bas_subjcode = Segments_AD_PostOm_subjcode(:,:,i) - mean(Segments_AD_PostOm_subjcode(:,1:99,i),2);
-    PostOm_3D_subjcode = cat(3,PostOm_3D_subjcode, Segment_PostOm_Bas_subjcode);
-end
-
-for i = 1:(size(Segments_AD_StimMoy_subjcode,3))
-    Segment_StimMoy_Bas_subjcode = Segments_AD_StimMoy_subjcode(:,:,i) - mean(Segments_AD_StimMoy_subjcode(:,1:99,i),2);
-    StimMoy_3D_subjcode = cat(3,StimMoy_3D_subjcode, Segment_StimMoy_Bas_subjcode);
-end
-
-for i = 1:(size(Segments_AD_Omission_subjcode,3))
-    Segment_Omission_Bas_subjcode = Segments_AD_Omission_subjcode(:,:,i) - mean(Segments_AD_Omission_subjcode(:,1:99,i),2);
-    Omission_3D_subjcode = cat(3,Omission_3D_subjcode, Segment_Omission_Bas_subjcode);
-end
 %%
-Baseline_Con = mean(Segments_AD_Con_subjcode(:,1:99,:),2)
-%%
-save '3D_data_subjcode' Con_3D_subjcode Fam_3D_subjcode Deviant_3D_subjcode Std_3D_subjcode PostOm_3D_subjcode StimMoy_3D_subjcode Omission_3D_subjcode
+save subjcode_3D_data_ML 'Con_3D_subjcode' 'StimMoy_3D_subjcode' 'Fam_3D_subjcode' 'Std_3D_subjcode' 'PostOm_3D_subjcode' 'Omission_3D_subjcode' 'Deviant_3D_subjcode'
 clear
